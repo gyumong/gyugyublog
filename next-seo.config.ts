79787b0ccdc01a439ec8813328ad2f5a66d4737a
gyumong <rlaalsrb3559@naver.com>
2023-04-23 10:54:08 +0900

    feat: default seo config 설정

---
+++ next-seo.config.ts
@@ -0,0 +1,14 @@
+import { NextSeoProps } from "next-seo";
+
+const config: NextSeoProps = {
+  openGraph: {
+    type: "website",
+    title: "Gyumong's Blog",
+    description: "김민규의 개발자아 블로그입니다.",
+    locale: "ko_KR",
+    url: "https://www.gyumong.dev/",
+    siteName: "gyumong.dev",
+  },
+};
+
+export default config;
