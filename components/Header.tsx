838ffea24dd44c1695f205bbc8eb9c68bc1401e5
gyumong <rlaalsrb3559@naver.com>
2023-04-23 16:40:45 +0900

    fix: dark mode 대응 및 간격 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,14 @@ const Header = () => {
   const { frontMatter } = useConfig();
   return (
     <>
-      <header className="flex flex-col items-center pt-32 pb-24 gap-10 sm:items-start sm:pt-10 sm:pb-4">
-        <h1 className="text-4xl text-gray-950">{frontMatter.title}</h1>
-        <time dateTime={frontMatter.date} className="text-sm text-slate-600">
+      <header className="flex flex-col items-center pt-32 pb-24 gap-10 sm:items-start sm:pt-10 sm:pb-8 sm:gap-4">
+        <h1 className="text-4xl text-gray-950 dark:text-white">
+          {frontMatter.title}
+        </h1>
+        <time
+          dateTime={frontMatter.date}
+          className="text-sm text-slate-600 dark:text-slate-200"
+        >
           {format(parseISO(frontMatter.date), "yyyy년 MM월 dd일")}
         </time>
       </header>
