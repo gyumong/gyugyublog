bbaa6aff7ec018bae558cab87913d134c553b1f3
gyumong <rlaalsrb3559@naver.com>
2023-04-26 22:32:36 +0900

    feat: ga setting + dev 메뉴 임시 내림

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,32 @@ import { ThemeProvider } from "next-themes";
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
 import { useUserEventLogger } from "@/hooks/useUserEventLogger";
+import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   useUserEventLogger();
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return (
     <>
       <DefaultSeo {...SEO} />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
