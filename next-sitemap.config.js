82e76eccf23b31b7b03c219e7634508f58a8c57e
gyumong <rlaalsrb3559@naver.com>
2024-10-05 00:12:27 +0900

    feat: sitemap 생성

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.gyumong.info',
+    generateRobotsTxt: true, // robots.txt 파일 생성 여부
+    sitemapSize: 7000, // sitemap의 최대 URL 개수
+    exclude: ['/404','*/_meta']
+};
